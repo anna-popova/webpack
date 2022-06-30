698d0892ebc8f9d8e99dc752479c64304c0eb654
Anna Popova <panasenko-anna.8@mail.ru>
2022-06-30 11:45:07 +0300

    fix problems

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+	},
+
 	module: {
 		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
 			{
 				test: /\.css$/,
 				use: [
@@ -13,11 +32,15 @@ module.exports = {
 			}
 		]
 	},
+
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: "./src/index.html",
 			filename: "./main.html"
 		}),
-		new MiniCssExtractPlugin(),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css',
+		}),
 	]
 }
